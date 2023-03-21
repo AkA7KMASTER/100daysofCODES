277ee783263429eefa1150a16013da2fabae77e7
Abhishek Maurya <114611419+AkA7KMASTER@users.noreply.github.com>
2023-03-21 21:35:12 +0530

    Update StrangeCounter.java

--- Day96/StrangeCounter.java
+++ Day96/StrangeCounter.java
@@ -8,6 +8,7 @@ class Result {
      * The function is expected to return a LONG_INTEGER.
      * The function accepts LONG_INTEGER t as parameter.
      */
+    
 
     public static long strangeCounter(long t) {
     // Write your code here
