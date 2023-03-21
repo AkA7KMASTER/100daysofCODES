00ffd403ba4781be7809252269a6066f591e4ca9
Abhishek Maurya <114611419+AkA7KMASTER@users.noreply.github.com>
2023-03-21 19:56:56 +0530

    Update TwoDArray.java

--- Day51/TwoDArray.java
+++ Day51/TwoDArray.java
@@ -37,47 +37,30 @@ import java.util.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.toList;
 
-class Result {
+public class maxhourglasssum {
+    public static void main(String[] args) {
+        int[][] arr = new int[][] { { 2, 1, 4, 3 }, { 0, 0, 5, 6 }, { 5, 6, 5, 7 }, { 7, 4, 3, 2 } };
+        int max = 0, ii = 0, jj = 0;
 
-    public static int hourglassSum(List<List<Integer>> arr) {   
-        int maxsum= Integer.MIN_VALUE;
-        int sum=0;
-        for(int i=0;i<4;i++){
-            for (int j=0;j<4;j++){
-                sum=arr.get(i).get(j)+arr.get(i).get(j+1)+arr.get(i).get(j+2)+arr.get(i+1).get(j+1)+arr.get(i+2).get(j)+arr.get(i+2).get(j+1)+arr.get(i+2).get(j+2);
-                if(sum>maxsum)
-                    maxsum=sum;    
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = 0; j < arr.length - 2; j++) {
+                int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j]
+                        + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                if (sum > max) {
+                    max = sum;
+                    ii = i;
+                    jj = j;
+                }
             }
         }
-        return maxsum;
-    }
-}
+        System.out.println("max sum = " + max);
+        System.out.println(arr[ii][jj] + " " + arr[ii][jj + 1] + " " + arr[ii][jj + 2] + "\n " + arr[ii + 1][jj + 1]
+                + "   \n" + arr[ii + 2][jj] + " " + arr[ii + 2][jj + 1] + " " + arr[ii + 2][jj + 2]);
 
-public class TwoDArray {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        List<List<Integer>> arr = new ArrayList<>();
-
-        IntStream.range(0, 6).forEach(i -> {
-            try {
-                arr.add(
-                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                        .map(Integer::parseInt)
-                        .collect(toList())
-                );
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-
-        int result = Result.hourglassSum(arr);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        System.out.println("(" + ii + "," + jj + ")" + "(" + ii + "," + (jj + 1) + ")" + "(" + ii + "," + (jj + 2)
+                + ")\n"
+                + "     (" + (ii + 1) + "," + (jj + 1) + ")\n" + "(" + (ii + 2) + "," + (jj) + ")" + "(" + (ii + 2)
+                + ","
+                + (jj + 1) + ")" + "(" + (ii + 2) + "," + (jj + 2) + ")");
     }
 }
