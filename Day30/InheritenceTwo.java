be6f07baca7425023fc8946c37514a554c92bf36
Abhishek Maurya <114611419+AkA7KMASTER@users.noreply.github.com>
2023-03-21 19:21:21 +0530

    Update InheritenceTwo.java

--- Day30/InheritenceTwo.java
+++ Day30/InheritenceTwo.java
@@ -1,11 +1,4 @@
 /*
-Question Link:https://www.hackerrank.com/challenges/java-inheritance-2
-Write the following code in your editor below:
-
-A class named Arithmetic with a method named add that takes 2 integers as parameters and returns an integer denoting their sum.
-A class named Adder that inherits from a superclass named Arithmetic.
-Your classes should not be be public.
-
 Input Format:
 You are not responsible for reading any input from stdin; 
 a locked code stub will test your submission by calling the add method on an Adder object and passing it  integer parameters.
