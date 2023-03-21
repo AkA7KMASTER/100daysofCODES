9499d4f93a25a4fc90aac84809b2b127d49ca756
Abhishek Maurya <114611419+AkA7KMASTER@users.noreply.github.com>
2023-03-21 19:52:08 +0530

    Update ReverseArray.java

--- Day50/ReverseArray.java
+++ Day50/ReverseArray.java
@@ -1,3 +1,19 @@
+/*
+Problem:
+An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, A, of size N, each memory location has some 
+unique index, (where 0 ≤ i ≤ N), that can be referenced as A[i](you may also see it written as Ai).
+
+Given an array, A, of N integers, print each element in reverse order as a single line of space-separated integers.
+Note: If you’ve already solved our C++ domain’s Arrays Introduction challenge, you may want to skip this.
+
+Sample Input
+
+4
+1 4 3 2
+
+Sample Output
+2 3 4 1
+*/
 import java.io.*;
 import java.math.*;
 import java.security.*;
