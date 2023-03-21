4c9cc21482a17ad0ccd3ed6fd86ce153e448e61f
Abhishek Maurya <114611419+AkA7KMASTER@users.noreply.github.com>
2023-03-21 20:19:03 +0530

    Update ReverseLL.java

--- Day59/ReverseLL.java
+++ Day59/ReverseLL.java
@@ -1,3 +1,49 @@
+/*
+Problem Statement:
+You are given the pointer to the head node of a linked list and you need to print all its elements in reverse order from
+tail to head, one element per line. The head pointer may be null meaning that the list is empty – in that case, do not
+print anything!
+
+Sample Input
+3
+5
+16
+12
+4
+2
+5
+3
+7
+3
+9
+5
+5
+1
+18
+3
+13
+
+Sample Output
+5
+2
+4
+12
+16
+9
+3
+7
+13
+3
+18
+1
+5
+
+Explanation
+There are three test cases.
+The first linked list has 5 elements: 16 -> 12 -> 4 -> 2 -> 5. Printing this in reverse order will produce: 5 -> 2 -> 4 -> 12 -> 16.
+The second linked list has 3 elements: 7 -> 3 -> 9. Printing this in reverse order will produce: 9 -> 3 -> 7.
+The third linked list has 5 elements: 5 -> 1 -> 18 -> 3 -> 13. Printing this in reverse order will produce: 13 -> 3 -> 18 -> 1 -> 5.
+*/
 import java.io.*;
 import java.util.stream.*;
 
