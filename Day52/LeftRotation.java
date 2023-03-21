d55f922cc9b47ce31267158502cdc131cf312a49
Abhishek Maurya <114611419+AkA7KMASTER@users.noreply.github.com>
2023-03-21 20:03:03 +0530

    Update LeftRotation.java

--- Day52/LeftRotation.java
+++ Day52/LeftRotation.java
@@ -1,3 +1,25 @@
+/*
+Problem Statement:
+A left rotation operation on an array of size n shifts each of the array’s elements 1 unit to the left. For example, if 
+left 2 rotations are performed on array [1,2,3,4,5], then the array would become [3,4,5,1,2].
+
+Given an array m of integers and a number, d, perform d left rotations on the array. Then print the updated array as a 
+single line of space-separated integers.
+
+Sample Input
+
+5 4
+1 2 3 4 5
+Sample Output
+
+5 1 2 3 4
+Explanation
+
+When we perform d = 4left rotations, the array undergoes the following sequence of changes:
+[1,2,3,4,5] --> [2,3,4,5,1] --> [3,4,5,1,2] --> [4,5,1,2,3] --> [5,1,2,3,4]
+Thus, we print the array’s final state as a single line of space-separated values, which is 5 1 2 3 4.
+
+*/
 import java.io.*;
 import java.util.*;
 import java.util.stream.*;
