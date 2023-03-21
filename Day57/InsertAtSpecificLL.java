5a72bd6fe0c537782033f2381e2f6ae87a87338e
Abhishek Maurya <114611419+AkA7KMASTER@users.noreply.github.com>
2023-03-21 20:14:38 +0530

    Update InsertAtSpecificLL.java

--- Day57/InsertAtSpecificLL.java
+++ Day57/InsertAtSpecificLL.java
@@ -1,3 +1,31 @@
+/*
+Problem Statement
+You’re given the pointer to the head node of a linked list, an integer to add to the list and the position at which the
+integer must be inserted. Create a new node with the given integer, insert this node at the desired position and return 
+the head node.
+
+A position of 0 indicates head, a position of 1 indicates one node away from the head and so on. The head pointer given 
+may be null meaning that the initial list is empty.
+
+As an example, if your list starts as 1 -> 2 -> 3 and you want to insert a node at
+position = 2
+data = 4
+your new list should be 1 -> 2 -> 4 -> 3 .
+
+Sample Input
+3
+16
+13
+7
+1
+2
+Sample Output
+16 13 1 7
+
+Explanation
+The initial linked list is 16 13 7. We have to insert at the position which currently has in it. The updated linked list will be 16 13 1 7.
+
+*/
 import java.io.*;
 import java.util.stream.*;
 
