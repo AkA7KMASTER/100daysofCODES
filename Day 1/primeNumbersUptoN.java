057dfb84aa56cdf0abe9d2edb8de4a0ead889a45
Abhishek Maurya <114611419+AkA7KMASTER@users.noreply.github.com>
2023-03-21 17:56:04 +0530

    Update primeNumbersUptoN.java

--- Day 1/primeNumbersUptoN.java
+++ Day 1/primeNumbersUptoN.java
@@ -1,4 +1,5 @@
 /*
+0. In this Prime number Program
 1. You've to print all prime numbers between a range. 
 2. Take as input "low", the lower limit of range.
 3. Take as input "high", the higher limit of range.
