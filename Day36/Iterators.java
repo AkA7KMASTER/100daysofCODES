94c1618b87c85dc03d4dc33cc7b4ef9e39178e65
Abhishek Maurya <114611419+AkA7KMASTER@users.noreply.github.com>
2023-03-21 19:30:11 +0530

    Update Iterators.java

--- Day36/Iterators.java
+++ Day36/Iterators.java
@@ -20,6 +20,7 @@ In this problem you need to complete a method func. The method takes an ArrayLis
 In that ArrayList there is one or more integer numbers, then there is a special string "###", 
 after that there are one or more other strings. A sample ArrayList may look like this:
 
+
 element[0]=>42
 element[1]=>10
 element[2]=>"###"
